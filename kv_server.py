5e3f391b854cfc075adfba39c58bf50063df9233
Krishna <krishnamurugan589@gmail.com>
2025-04-13 13:29:51 +0530

    feat: server implementation of kv store

---
+++ kv_server.py
@@ -0,0 +1,31 @@
+from flask import Flask, request
+from persistent_kv_store import PersistentKVStore
+
+app = Flask(__name__)
+store = PersistentKVStore("server_store.json")
+
+
+@app.route("/kv/<key>", methods=['GET'])
+def get_value(key: str):
+    value = store.get(key)
+    if not value:
+        return "key not found", 404
+    return f"key: {key}, value: {value}"
+
+@app.route("/kv/<key>", methods=['PUT'])
+def put_value(key: str):
+    value = request.get_data(as_text=True)
+    if not value:
+        return "value should be None", 400
+    store.put(key, value)
+    return f"status: success, key: {key}"
+
+@app.route("/kv/<key>", methods=['DELETE'])
+def delete_key(key: str):
+    success = store.delete(key)
+    if not success:
+        return f"key: {key} not found"
+    return f"success: true, key: {key}"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
